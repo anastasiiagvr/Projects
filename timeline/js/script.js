f547fc0c523d9b3eb340a1e5806d84cf9856798e
Anastasiia <gvr.996@gmail.com>
2019-12-03 23:28:08 +0200

    replace banner image

--- timeline/js/script.js
+++
@@ -1,9 +0,0 @@
-let comments = document.getElementsByClassName('tweet');
-
-  for (comment of comments) {
-    comment.addEventListener('mouseenter', function(event){
-      event.target.style.backgroundColor = "gray";}, false);
-
-    comment.addEventListener('mouseleave', function(event){
-      event.target.style.backgroundColor = "";}, false);
-  }
